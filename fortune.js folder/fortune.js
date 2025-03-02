3abe029c5bcdaab8cfe247aceafbe3fa8e3ad23d
felisha895 <junebug20062020@gmail.com>
2025-03-02 00:55:07 -0600

    Add files via upload

---
+++ fortune.js folder/fortune.js
@@ -0,0 +1,11 @@
+// Jacob Phillips, 3.1.2025
+
+let career = "ride operator";
+let childrenNumber = 0;
+let partnerName = "no one";
+let jobLocation = "Kansas City";
+let salary = "$31,200";
+
+let result = ("You will be a" + " " + career + " " + "in" + " " + jobLocation + ", " + "and married to" + " " + partnerName + " " + "with" + " " + childrenNumber + " " + "kids with a salary of" + " " + salary + ".");
+
+console.log(result);
