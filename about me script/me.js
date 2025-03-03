148050371ce117dda513ebe8131cebed1272a0cb
felisha895 <junebug20062020@gmail.com>
2025-03-02 18:35:03 -0600

    Add files via upload

---
+++ about me script/me.js
@@ -0,0 +1,11 @@
+// Jacob Phillips, 3.2.2025
+
+let name = "Jacob Phillips";
+let favoriteColor = "blue";
+let hobby = "karate and cycling";
+let favoriteFood = "chicken fries and pizza";
+
+console.log("My name is" + " " + name + ".");
+console.log("My favorite color is" + " " + favoriteColor + ".");
+console.log("My hobbies are especially" + " " + hobby + ".");
+console.log("My favorite food is" + " " + favoriteFood + ".");
